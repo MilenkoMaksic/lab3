6ad494b0bb96af2a3fa83e3522398788e071a12f
MilenkoMaksic <maksicmra11@gmail.com>
2019-03-18 12:11:30 +0100

    initial commit

---
+++ drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
@@ -0,0 +1,11 @@
+##############################################################################
+## Filename:          D:\ra11-2016\lab3/drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
+## Description:       Microprocess Driver Command (tcl)
+## Date:              Mon Mar 18 12:05:56 2019 (by Create and Import Peripheral Wizard)
+##############################################################################
+
+#uses "xillib.tcl"
+
+proc generate {drv_handle} {
+  xdefine_include_file $drv_handle "xparameters.h" "my_peripheral" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" 
+}
---
+++ drivers/my_peripheral_v1_00_a/src/my_peripheral.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          D:\ra11-2016\lab3/drivers/my_peripheral_v1_00_a/src/my_peripheral.c
+* Version:           1.00.a
+* Description:       my_peripheral Driver Source File
+* Date:              Mon Mar 18 12:05:56 2019 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "my_peripheral.h"
+
+/************************** Function Definitions ***************************/
+
